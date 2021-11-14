2610cfc1a6ac3b37917a9a0392632bbafdd0af42
Aditya Mishra <34389531+31aditya0193@users.noreply.github.com>
2021-11-14 17:18:25 +0530

    Initial Commit

---
+++ SwiftUI_Experiments/App/SwiftUI_ExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExperimentsApp.swift
+//  SwiftUI_Experiments
+//
+//  Created by Aditya Mishra on 14/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI_Experiments/View/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  SwiftUI_Experiments
+//
+//  Created by Aditya Mishra on 14/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
